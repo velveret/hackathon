c5350f6dba2d1af8b314bf07ec8fa1551fd3716d
Roger Curley <palmtree3000@gmail.com>
2012-08-27 14:53:41 -0400

    Fixed stupid bug: now works for SHO.

--- Physics_Calculator.py
+++ Physics_Calculator.py
@@ -15,8 +15,8 @@ def solveSymbolic(L, coordinates):
 	coordinates_t=[]
 	for (i,(q,qdot)) in enumerate(coordinates):
 		q_t=sp.Function('q%i'%i)
-		dL_dq=sp.diff(L,q).subs({q:q_t,qdot:sp.diff(q_t(t),t)})
-		dL_dqdot=sp.diff(L,qdot).subs({q:q_t,qdot:sp.diff(q_t(t),t)})
+		dL_dq=sp.diff(L,q).subs({q:q_t(t),qdot:sp.diff(q_t(t),t)})
+		dL_dqdot=sp.diff(L,qdot).subs({q:q_t(t),qdot:sp.diff(q_t(t),t)})
 		ddL_dqdot_dt=sp.diff(dL_dqdot,t)
 		eulerLagrange.append((ddL_dqdot_dt-dL_dq))
 		coordinates_t.append(q_t)
