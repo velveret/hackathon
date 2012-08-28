f8b6e3e90aad32cfb03dd65e8b8665c748afba20
vickyl <vickyl1@server>
2012-08-27 22:09:09 -0400

    fixed by replacing prev with dict of parents and fixed get_unvisited to use find_head

--- comp_coords_01.py
+++ comp_coords_01.py
@@ -6,7 +6,6 @@ Created on Mon Aug 27 18:42:43 2012
 """
 
 from node_02 import *
-from sets import Set
 
 # Given dictionary (graph), compute coordinates of nodes
 #def comp_coords_01(diagram, nodeSet):
@@ -20,39 +19,37 @@ def find_head(diagram):
     return diagram.keys()[0]
 
 def get_unvisited(diagram, visited):
-    visitedSet = Set(visited)
-    nodeSet = Set(diagram.keys())
-    unvisited = nodeSet.difference(visitedSet)
-    return unvisited.pop()
+    diagramCopy = copy.deepCopy(diagram)
+    for node in visited:
+        del diagramCopy[node]
+    return find_head(diagramCopy)
 
 def dfs(diagram, head):
     visited = []
+    parents = {}
     next = []
     next.append(head)
-    prev = None
+    parents[head] = None
     while len(visited) < len(diagram.keys()):
         if len(next) == 0:
             next.append(get_unvisited(diagram, visited))
-            prev = None
+            parents[head] = None
             continue
         else:
             current = next.pop()
             visited.append(current)
-            print "visiting %s with previous %s" % (current.x, prev.x if prev else None)
-            if not current.fixed:
-                # process here
-                print "processing"
-                
+            print "visiting %s with parent %s" % (current.x,\
+                parents[current].x if parents[current] else None)
             for child in diagram[current]:
                 if not (child in visited or child in next):
                     next.append(child)
-            prev = current
-
+                    parents[child] = current
+    return parents
                 
-n1 = Mass('x_1','y_1','xd_1','yd_1',1.0,False)
-n2 = Segment('x_2','y_2','xd_2','yd_2','th_2','thd_2',2.0,1.0,0.5,1.0,True)
-n3 = Segment('x_3','y_3','xd_3','yd_3','th_3','thd_3',2.0,1.0,0.5,1.0,False)
+n1 = Mass('x_1','y_1','xd_1','yd_1',"1",1.0,False)
+n2 = Segment('x_2','y_2','xd_2','yd_2','th_2','thd_2',"2", 2.0,1.0,0.5,1.0,True)
+n3 = Segment('x_3','y_3','xd_3','yd_3','th_3','thd_3',"3", 2.0,1.0,0.5,1.0,False)
 
 Diagram = {n1:[n2, n3], n2:[n1], n3:[n1]}
 head = find_head(Diagram)
-dfs(Diagram, head)
+print dfs(Diagram, head)
